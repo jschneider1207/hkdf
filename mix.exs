c3e2a0cf0690f936a40f9a466c500a141fa78cca
Jessica Schneider <jessica.schneider1207@gmail.com>
2022-01-28 22:48:08 -0500

    wrong version number

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule HKDF.Mixfile do
 
   def project do
     [app: :hkdf,
-     version: "1.0.0",
+     version: "0.2.0",
      build_path: "_build",
      config_path: "config/config.exs",
      deps_path: "deps",
